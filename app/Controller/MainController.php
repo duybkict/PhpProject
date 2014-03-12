954efab1e805de6f66fe98b2c63ec38c9fb33269
Duy Trần <duytran.bkict@gmail.com>
2014-03-12 11:56:21 +0700

    Init commit

---
+++ app/Controller/MainController.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MainController
+ *
+ * @author Duy Tran
+ */
+class MainController extends AppController {
+	//put your code here
+	public function index() {
+				
+	}
+}
+
+?>
---
+++ app/View/Main/index.ctp
@@ -0,0 +1,2 @@
+<h1>aaaaaa</h1>
+<?php echo $this->Html->image("cake.icon.png");
